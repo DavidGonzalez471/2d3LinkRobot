2b1fcd6ea0410c6d068ae2e2a6f05af2bc6c21dc
David Gonzalez <88919781+DavidGonzalez471@users.noreply.github.com>
2023-03-30 18:02:08 -0400

    Update 2dRobot.py

--- 2dRobot.py
+++ 2dRobot.py
@@ -16,14 +16,3 @@ hand=[0,300]
 
 table_vals=[]
 
-#Values for the table
-Bpos=[0,0]
-AB_angle = 0
-AB_angle_diff = 0
-Cpos=[0,0]
-BC_angle=[0,0]
-BC_angle_diff=[0,0]
-Dpos=[0,0]
-CD_angle=0
-CD_angle_diff=0
-Max_angle_diff=0
